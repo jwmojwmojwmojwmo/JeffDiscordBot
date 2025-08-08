63aac66ebd28af3cc54b954a070ad27dc4e7cab9
Jason Wang <jwmo@students.cs.ubc.ca>
2025-08-08 12:54:30 -0600

    Initial Commit: JeffBot v1

---
+++ commands/utility/jeff.js
@@ -0,0 +1,46 @@
+const { AttachmentBuilder, MediaGalleryBuilder, MessageFlags, SlashCommandBuilder } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const assetsDir = path.join(__dirname, "..", "..", 'assets');
+
+let file = new AttachmentBuilder('assets/jeff.webp');
+
+let gallery = new MediaGalleryBuilder()
+    .addItems(
+        mediaGalleryItem => mediaGalleryItem
+            .setURL('attachment://jeff.webp')
+    );
+
+function getFile() {
+    let fileName = "";
+    fs.readdir(assetsDir, (err, files) => {
+        if (err) {
+            console.error('Failed to read assets folder:', err);
+            return;
+        }
+
+        if (files.length === 0) {
+            console.log('No files in assets folder.');
+            return;
+        }
+
+        const randomIndex = Math.floor(Math.random() * files.length);
+        fileName = files[randomIndex];
+        file = new AttachmentBuilder("assets/" + fileName);
+        gallery = new MediaGalleryBuilder().addItems(mediaGalleryItem => mediaGalleryItem.setURL('attachment://' + fileName));
+    });
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('jeff')
+        .setDescription('Gives Jeff!'),
+    async execute(interaction) {
+        getFile();
+        await interaction.reply({
+            components: [gallery],
+            files: [file],
+            flags: MessageFlags.IsComponentsV2,
+        });
+    },
+};
---
+++ commands/utility/jeffspeak.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder, bold } = require('discord.js');
+
+const msgs = ["MRRRR! [ravenous]", "MRRRR! [vicious]", "Mrrr...[sad]", "Mrrrr! [eager]", "Mrrrr! [urgent]",
+    "Mrrrr! [mocking]", "Mrrrr! [startling]", "Mrrrr! [playful]", "Mrrrr! [supportive]", "Mrrrr... [growling]", "Mrrrr…? [curious]",
+    "Mrrrr?! [terrified]", "Mrrrr! Mrrrr. MRRRR! [excited]", "Mrrrr! [triumphant]", "Mrrrr… [snarling]", "Mrrrr! [apologetic]", "MRRAAAARR!",
+    "Mrrrr! [shocked]", "Mrrrr! [sad]", "Uh-Oh! [sad]", "Ah-Ha! [supportive]", "Woop woop! [impressed]", "Mrrrr! [relieved]", "Mrrrr! [happy]",
+    "Mrrrr -- ? [confused]", "Mrrrr… [frustrated]", "Mrrrr! [in agreement]", "No!", "Uh-Uh!", "Mrrrr! [beckoning]", "Mrrrr! [friendly]", "Mrrrr... [worried]",
+    "Mrrrr... [dismissive]", "Mrrrr... [impatient]", "Mrrrr! [satisfied]", "No! [urgent]", "MRRR! [panicked]"];
+
+function getMsg() {
+    return bold(msgs[Math.floor(Math.random() * msgs.length)]);
+}
+
+function fullMsg() {
+    let msg = "";
+    const msgNum = Math.floor(Math.random() * 6) + 1;
+    for (let i = 0; i <= msgNum; i++) {
+        msg = msg + "\n" + getMsg();
+    }
+    return msg;
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('jeffspeak')
+        .setDescription('Say something to Jeff')
+        .addStringOption(option =>
+            option
+                .setName('phrase')
+                .setDescription('What you want to say to Jeff')
+                .setRequired(true)),
+    async execute(interaction) {
+        await interaction.reply(interaction.user.globalName + " says: " + interaction.options.getString('phrase') + "\n\n"
+            + "Jeff says:" + fullMsg());
+    },
+};
